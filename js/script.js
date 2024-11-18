8878841faf9978cd99f7da76f7252a7e5541ed90
khankekee <phukhanhk2@gmail.com>
2024-11-18 20:10:57 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const favoriteBtn = document.querySelector('#favorite-btn');
+
+    // Khi người dùng bấm vào nút "Sở thích"
+    favoriteBtn.addEventListener('click', () => {
+        window.location.href = 'favorites.html'; // Chuyển đến trang sở thích
+    });
+});
