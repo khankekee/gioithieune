8fe2b1fb7da1de9ef8b2efb3c8c0428eefeb4a1f
khankekee <phukhanhk2@gmail.com>
2024-11-19 20:12:58 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const favoriteBtn = document.querySelector('#favorite-btn');
 
-    // Khi người dùng bấm vào nút "Sở thích"
-    favoriteBtn.addEventListener('click', () => {
-        window.location.href = 'favorites.html'; // Chuyển đến trang sở thích
-    });
-});
